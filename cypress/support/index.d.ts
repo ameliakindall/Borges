8f104a3b53c820221e5804ad23d2053c491f59ec
Amelia Kindall <amelia.kindall@gmail.com>
2019-08-18 15:28:57 -0500

    DOC-CUSTOM edit notes for login command to specify what the return value is

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,9 +1,9 @@
 declare namespace Cypress {
   interface Chainable {
     /**
-     * Custom command to login via POST request to the ?controller=authentication route
+     * Custom command to login via POST request to the controller=authentication route
      * @example cy.login('username@bogus.com', 'mypassword')
     */
-    login(value: string): Chainable<Element>
+    login(username: string, password: string): Response
   }
 }
