87c1873362678c5fc16c9ae6b04b9f060bc61b42
Amelia Kindall <amelia.kindall@gmail.com>
2019-08-26 18:10:22 -0500

    LOGIN-ELEMENTS test that login form and forgot password link is visible and that it links to /?controller=password

--- cypress/integration/login_form.spec.js
+++ cypress/integration/login_form.spec.js
@@ -1,15 +1,21 @@
 /// <reference types="cypress" />
 /// <reference path="../support/index.d.ts" />
 
-describe("Login form", () => {
+describe( "Login form", () => {
 
   beforeEach( () => {
-    cy.visit("/?controller=authentication&back=my-account")
-  })
+    cy.visit( "/?controller=authentication&back=my-account" )
+  } )
 
-  context("Form elements", () => {
-    it("Links to /?controller=password", () => {
-      cy.get("#login_form").should("be.visible")
-    })
-  })
-})
+  context( "Form elements", () => {
+
+    it( "Links to /?controller=password", () => {
+      cy.get( "#login_form" ).within( ( form ) => {
+        expect( form ).to.be.visible
+        cy
+          .contains( "Forgot your password?" )
+          .should( "have.attr", "href", `${Cypress.config().baseUrl}?controller=password` )
+      } )
+    } )
+  } )
+} )
