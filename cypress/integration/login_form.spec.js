f30b944f0086a84e9701dba1c72f217776d2ab8e
Amelia Kindall <amelia.kindall@gmail.com>
2019-08-26 16:44:05 -0500

    LOGIN-ELEMENTS added a context function for login form elements

--- cypress/integration/login_form.spec.js
+++ cypress/integration/login_form.spec.js
@@ -2,9 +2,17 @@
 /// <reference path="../support/index.d.ts" />
 
 describe("Login form", () => {
+
   beforeEach( () => {
     cy.visit("/?controller=authentication&back=my-account")
   })
+
+  context("Form elements", () => {
+    it("Links to /?controller=password", () => {
+
+    })
+  })
+
   it("test that the before hook works", () => {
     cy.get("#login_form").should("be.visible")
   })
