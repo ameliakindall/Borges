32cec847a24f3fa4af78df1e6db12fbeaae469b6
Amelia Kindall <amelia.kindall@gmail.com>
2019-09-16 18:23:26 -0500

    ADDTO-CART add typescript reference to top of summary spec and set up a dummy test so I can investigate whether or not there are any global functions that I can use

---
+++ cypress/integration/checkout/summary.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+/// <reference path="../../support/index.d.ts" />
+
+describe( "Checkout: Summary step", () => {
+
+  it( "This is a dummy test", () => {
+    cy.visit( 'index.php?' )
+    cy.window()
+  } )
+} )
